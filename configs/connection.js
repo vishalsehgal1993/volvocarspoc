1ca5e4e5ee48a926fbc07b5e342948a662ce5e48
unknown <vishalsehgal.skrm@gmail.com>
2021-09-26 21:16:10 +0530

    basic graphql projects with connection

---
+++ configs/connection.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+var config = require("../configs/development.json");
+const color = require('colors');
+
+mongoose.Promise = Promise;
+mongoose.connection.on('connected', () => {
+  console.log(color.green('Connection Established'))
+})
+
+mongoose.connection.on('reconnected', () => {
+  console.log(color.green('Connection Reestablished'))
+})
+
+mongoose.connection.on('disconnected', () => {
+  console.log(color.red('Connection Disconnected'))
+  mongoose.connection.close().then(()=>{
+    console.log(color.red('Connection Closed Explicitly: '));
+  }).catch((er)=>{
+    console.log(color.red('Connection Closed Explicitly: ' , er));
+  })
+})
+
+mongoose.connection.on('close', () => {
+  console.log(color.red('Connection Closed'))
+})
+
+mongoose.connection.on('error', (error) => {
+  console.log('ERROR: ' + error);
+  mongoose.connection.close().then(()=>{
+    console.log(color.red('Connection Closed Explicitly: '));
+  }).catch((er)=>{
+    console.log(color.red('Connection Closed Explicitly: ' , er));
+  })
+})
+
+const connectToDb = async (app,port) => {
+  const dbConnection = 
+  config.dev.DIALECT + 
+  config.dev.USERNAME +
+  config.dev.PASSWORD + 
+  config.dev.HOST + 
+  config.dev.PARAMS;
+  await mongoose.connect(dbConnection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then((data)=>{
+    app.listen(port, () => {
+      console.log(color.green(`Application Running Listening on port: ${port}`));
+    });  
+  })
+}
+
+
+module.exports = { connectToDb };
--- index.js
+++ index.js
@@ -1,19 +1,86 @@
+'use-strict'
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const schema = require('./models')
+const graphqlHttp = require('express-graphql');
+const cors = require("cors");
 var configs = require('./configs/development.json');
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+const { connectToDb } = require("./configs/connection");
+const color = require('colors');
 const port = configs.dev.PORT || 3000;
 const app = express();
+app.disable('e-tag').disable('x-powered-by');
+
+//CORS configs
+var corsOptions = {
+    origin: "*",
+    methods: ["GET", "PUT", "POST", "PUT", "PATCH" , "DELETE", "OPTIONS"],
+    allowedHeaders: [
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+        "Authorization",
+        "X-Domain"
+
+    ],
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+const start = async()=> 
+{
+    connectToDb(app,port)
+    .catch(error => {
+        console.error("error while connection is:", error)
+    })
+}
+
+process.on('unhandledRejection', error => 
+{
+    console.error('Uncaught Error', error);
+});
+
+function onError(error) 
+{
+    if (error.syscall !== 'listen') 
+    {
+        throw error;
+    }
+    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+    switch (error.code) 
+    {
+        case 'EACCES':
+        console.error(color.red(bind + ' requires elevated privileges'));
+        process.exit(1);
+        break;
+        case 'EADDRINUSE':
+        console.error(color.red(bind + ' is already in use'));
+        process.exit(1);
+        break;
+        default:
+        throw error;
+    }
+}
+  
+function onListening() 
+{
+  console.log(color.green("Server is Listening" ));
+}
+
+app.on('error', onError);
+app.on('listening', onListening);
 
 //This route will be used as an endpoint to interact with Graphql,All queries will go through this route. 
-app.use('/bookings', graphqlHTTP({
-    //directing express-graphql to use this schema to map out the graph 
-    schema,
-    //directing express-graphql to use graphiql when goto '/graphql' address in the browser
-    //which provides an interface to make GraphQl queries
-    graphiql:true
-}));
+app.use(
+    '/graphql',
+    graphqlHttp({
+      schema: graphQlSchema,
+      rootValue: graphQlResolvers,
+      graphiql: true
+    })
+);
 
-app.listen(port, () => {
-    console.log(`Application Running Listening on port: ${port}`);
-});
+start()
+
